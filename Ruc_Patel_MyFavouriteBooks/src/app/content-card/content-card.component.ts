477341051c8272d54925db440a6d9dd5d07506c2
Ruchita495 <122808258+Ruchita495@users.noreply.github.com>
2024-01-27 11:36:54 -0500

    Update content-card.component.ts

--- Ruc_Patel_MyFavouriteBooks/src/app/content-card/content-card.component.ts
+++ Ruc_Patel_MyFavouriteBooks/src/app/content-card/content-card.component.ts
@@ -17,7 +17,7 @@ export class ContentCardComponent {
       title: 'Bhagavad Gita',
       description: 'The Bhagavad Gita is set in a narrative framework of dialogue between the Pandava prince Arjuna and his charioteer guide Krishna, an avatar of Vishnu. At the start of the Kurukshetra War between the Pandavas and the Kauravas.',
       creator: 'Vyasa',
-      imgURL: 'the-bhagavad-gita.jpg',
+      imgURL: 'https://www.shutterstock.com/image-photo/utter-pardesh-india-bhagavad-gita-600nw-1848771181.jpg',
       type: 'Spiritual',
       tags: ["Sanskrit","Multilanguage"],
     });
@@ -26,7 +26,7 @@ export class ContentCardComponent {
       title: 'Three Thousand Stitches',
       description: 'hree Thousand Stitches is a book written by Sudha Murthy and published in 2017. The book is a collection of 11 different stories, which she draws from her personal life, with a message engraved in every story.',
       creator: 'Sudha Murthy',
-      imgURL: 'Three_Thousand_Stitches.jpg',
+      imgURL: 'https://1.bp.blogspot.com/-S_260M3Og84/XOLk4Ahl7jI/AAAAAAAAAhE/rMkBx1giYkst8ee-I69R_89tqQkjKGvlQCLcBGAs/s1600/IMG_20190520_203356_935.jpg',
       type: 'Motivational',
       tags: ["self-sustainable","personal experiences"],
     });
@@ -35,7 +35,7 @@ export class ContentCardComponent {
       title: 'Avengers',
       description: 'The Avengers are an all-star ensemble cast of established superhero characters from the Marvel Comics portfolio. Diegetically, these superheroes usually operate independently but occasionally assemble as a team to tackle especially formidable villains.',
       creator: 'Stan Lee ,Jack Kirby',
-      imgURL: 'Avengers.jpg',
+      imgURL: 'https://www.cardboardmemories.ca/cdn/shop/products/347a3d800381f5ce90967d1b2cdacfb5_700x700.jpg?v=1693984901',
       type: 'Comic',
       tags: ["Captain Marvel","Captain America","Iron Man"],
     });
